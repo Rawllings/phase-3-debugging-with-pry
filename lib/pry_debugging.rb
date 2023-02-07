12e2fd9d580c757ceef6626c6af6ffec8641b7f2
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-07 22:17:58 +0300

    Pry debugging

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -2,3 +2,40 @@ def plus_two(num)
 	num + 2
 	num
 end
+
+def reverse_each_word(sentence)
+	words = sentence.split
+	reversed_words = []
+  
+	words.each do |word|
+	  reversed_words << word.reverse
+	end
+  
+	reversed_words.join(" ")
+  end
+
+
+
+  def reverse_each_word(sentence)
+	words = sentence.split
+  
+	reversed_words = words.map do |word|
+	  word.reverse
+	end
+  
+	reversed_words.join(" ")
+  end
+
+
+
+  def reverse_each_word(sentence)
+	words = sentence.split
+  
+	reversed_words = words.map(&:reverse)
+  
+	reversed_words.join(" ")
+  end
+
+  def reverse_each_word(sentence)
+	sentence.split.map(&:reverse).join(" ")
+  end
